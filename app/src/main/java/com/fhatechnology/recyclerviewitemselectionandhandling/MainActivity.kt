f5e4a76ec9a5175dd0ac43b59923830e33777cd9
Awais <m.awaisnaxeer@gmail.com>
2021-03-04 20:48:42 +0500

    RecyclerView item Selection

--- app/src/main/java/com/fhatechnology/recyclerviewitemselectionandhandling/MainActivity.kt
+++ app/src/main/java/com/fhatechnology/recyclerviewitemselectionandhandling/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.fhatechnology.recyclerviewitemselectionandhandling
 
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StableIdKeyProvider
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,13 +18,18 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerview:RecyclerView
-    private val adapter = MainAdapter()
+    private val adapter = MainAdapter(this)
+    lateinit var txt_selected:TextView
+
+
+    var tracker: SelectionTracker<Long>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        txt_selected = findViewById(R.id.txt_selected)
 
         recyclerview = findViewById(R.id.recyclerview)
         val linearLayoutManager = LinearLayoutManager(this)
@@ -27,10 +40,35 @@ class MainActivity : AppCompatActivity() {
         recyclerview.addItemDecoration(dividerItemDecoration)
         adapter.list = createRandomIntList()
         adapter.notifyDataSetChanged()
+        tracker = SelectionTracker.Builder<Long>(
+                "mySelection",
+                recyclerview,
+                MyItemKeyProvider(recyclerview),
+                MyItemDetailsLookup(recyclerview),
+                StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(
+                SelectionPredicates.createSelectAnything()
+        ).build()
+        adapter.tracker = tracker
 
+        tracker?.addObserver(
+                object : SelectionTracker.SelectionObserver<Long>() {
+                    override fun onSelectionChanged() {
+                        super.onSelectionChanged()
+                        val items = tracker?.selection!!
+                        if (items.size()>0){
+                            findViewById<RelativeLayout>(R.id.selected_txt_layout).visibility = View.VISIBLE
+                            for ((index,value) in items.withIndex()){
+                                txt_selected.append("${value.toString()}\n")
+                            }
+                            txt_selected.append("       #################\n")
+
+                        }
+                    }
+                })
     }
 
     private fun createRandomIntList(): List<Int> {
-        return (1..99).map { Random.nextInt() }
+        return (1..99).map { it*2 }
     }
 }
--- app/src/main/java/com/fhatechnology/recyclerviewitemselectionandhandling/MainAdapter.kt
+++ app/src/main/java/com/fhatechnology/recyclerviewitemselectionandhandling/MainAdapter.kt
@@ -1,21 +1,40 @@
 package com.fhatechnology.recyclerviewitemselectionandhandling
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.RecyclerView
 
-class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter(var context:Context) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     var list: List<Int> = arrayListOf()
+var tracker: SelectionTracker<Long>? = null
+
+    init {
+        setHasStableIds(true)
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image_selector:ImageView = itemView.findViewById(R.id.image_selector)
+         var txt_number: TextView = itemView.findViewById(R.id.txt_number)
+        fun bind(value: Int, isActivated: Boolean = false) {
+            txt_number.text = value.toString()
+            itemView.isActivated = isActivated
 
-        private var txt_number: TextView = itemView.findViewById(R.id.txt_number)
-        fun bind(number: Int) {
-            txt_number.setText("${number}")
+
         }
+
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<Long> =
+            object : ItemDetailsLookup.ItemDetails<Long>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): Long? = itemId
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,10 +45,38 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val number = list[position]
-        holder.bind(number)
+        tracker?.let {
+            holder.bind(number, it.isSelected(position.toLong()))
+        }
+
+        holder.itemView.setOnClickListener {
+            if (!tracker!!.hasSelection()){
+                Toast.makeText(context, "single clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        tracker?.let {
+            if (it.isSelected(position.toLong()) )  {
+                it.select(position.toLong())
+                holder.image_selector.visibility = View.VISIBLE
+                holder.txt_number.textSize = 28F
+                //changing the  color of the clicked/selected item to light gray
+                //parent.setBackgroundColor( ContextCompat.getColor(context, R.color.extra_light_gray))
+            } else {
+                it.deselect(position.toLong())
+                // set color white
+                holder.image_selector.visibility = View.GONE
+                //parent.setBackgroundColor( ContextCompat.getColor(context, R.color.white))
+            }
+        }
+
+
+
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
+
+    override fun getItemId(position: Int): Long = position.toLong()
 }
